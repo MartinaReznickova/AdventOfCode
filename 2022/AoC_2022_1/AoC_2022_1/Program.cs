d3e27788559ba867cc04bec12726779a563208b4
Martina Řezníčková <reznickova.mart@gmail.com>
2023-11-16 17:22:29 +0100

    AoC_2022_1

---
+++ 2022/AoC_2022_1/AoC_2022_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace AoC_2022_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "data.txt";
+          
+
+            List<int> reindeerFood = new List<int>();
+            int sumForOneReindeer = 0;
+            int maxSum = 0;
+            string line = "";
+
+            StreamReader sr = new StreamReader(filepath);
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(line) == false)
+                {
+                    reindeerFood.Add(int.Parse(line.Trim()));
+                }
+
+                else
+                {
+                    sumForOneReindeer = reindeerFood.Sum();
+                    if (maxSum < sumForOneReindeer)
+                    {
+                        maxSum = sumForOneReindeer;
+                    }
+
+                    reindeerFood.Clear();
+                    sumForOneReindeer = 0;
+                }
+            }
+
+            Console.WriteLine("{0}", maxSum);
+        }
+    }
+}
