85c5f59fed4300f5e24d266b7a7c8e4eee448be2
Martina Řezníčková <reznickova.mart@gmail.com>
2023-12-05 21:34:19 +0100

    day 1

---
+++ 2023/AoC2023_Day1/AoC2023_Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AoC2023_Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "data1.txt";
+            int firstNum = 0;
+            int secondNum = 0;
+            int num = 0;
+            long sum = 0;
+            string numbers = "0123456789";
+
+
+
+            StreamReader sr = new StreamReader(filepath);
+
+            string line = "";
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                var indexFirstNum = line.IndexOfAny(numbers.ToCharArray());
+                firstNum = int.Parse(line[indexFirstNum].ToString());
+                line.Reverse();
+                var indexSecondNum = line.IndexOfAny(numbers.ToCharArray());
+                secondNum = int.Parse(line[indexSecondNum].ToString());
+
+                string twoNumbers = firstNum.ToString() + secondNum.ToString();
+
+                num = int.Parse(twoNumbers);
+
+                sum += num;
+
+                num = 0;
+                firstNum = 0;
+                secondNum = 0;
+
+
+
+
+                
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
--- 2023/AoC2023_Day1/AoC2023_Day1/obj/Debug/net8.0/AoC2023_Day1.AssemblyInfo.cs
+++ 2023/AoC2023_Day1/AoC2023_Day1/obj/Debug/net8.0/AoC2023_Day1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AoC_2022_1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AoC2023_Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("AoC_2022_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AoC_2022_1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f625b09a8080480d4e2d5b67e1685423d4e93699")]
+[assembly: System.Reflection.AssemblyProductAttribute("AoC2023_Day1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AoC2023_Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
